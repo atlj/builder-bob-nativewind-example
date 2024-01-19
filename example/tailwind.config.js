092835b129cf7695ebdf8fd9d4702f0ea73722c0
Burak Güner <burak.guner@callstack.com>
2024-01-19 17:47:30 +0300

    setup nativewind for library

--- example/tailwind.config.js
+++ example/tailwind.config.js
@@ -1,10 +1,17 @@
+// const libraryPath = require.resolve('react-native-nativewind-example/package.json');
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
     // Example app's own components
     './src/**/*.{js,jsx,ts,tsx}',
-    // We use metro resolver plugin to add an alias for the library. Therefore, we need to add the alias path to the content array.
-    '../src/**/*.{js,jsx,ts,tsx}'],
+    // Since we are using metro resolver for development, we need to add the library's source. You should delete this line
+    // if you are installing the library as a dependency to another project
+    '../src/**/*.{js,jsx,ts,tsx}',
+    // If you are installing the library as a dependency, you'll want to uncomment this line
+    // `${libraryPath}/src/**/*.{js,jsx,ts,tsx}`,
+  ],
+
   theme: {
     extend: {},
   },
